80a0af94b45d8211b53f83ac741d616862e7d75c
Dmitry Novotochinov <trybeee@gmail.com>
2014-09-11 22:53:20 +0000

    add submit method to Form

--- lib/hubspot/form.rb
+++ lib/hubspot/form.rb
@@ -7,5 +7,11 @@ module Hubspot
     def url
       Hubspot::Utils.generate_url("/uploads/form/v2/:portal_id/:form_guid", {form_guid: @form_guid}, {base_url: "https://forms.hubspot.com", hapikey: false})
     end
+
+    def submit(params={})
+      resp = HTTParty.post(url, body: params)
+      raise(Hubspot::RequestError.new(resp, "Cannot create contact with email: #{email}")) unless resp.success?
+      resp.body
+    end
   end
 end
